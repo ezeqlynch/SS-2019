4e0e8bca3cdf4c3ddbad23f6ee2fbaa4db9b013d
Teófilo Duffau <tduffau@itba.edu.ar>
2019-03-15 09:36:11 -0300

    Add parser to plotter

--- TP0/analysis.py
+++ TP0/analysis.py
@@ -1,43 +1,36 @@
+import argparse
+from argparse import RawTextHelpFormatter
 from particle import Particle
 from markerupdater import MarkerUpdater
 import matplotlib.pyplot as plt
 import numpy as np
-import mplcursors
 
-my_updater = MarkerUpdater()
 
-staticFile = open("data/output.txt", "r")
-particles = list()
-particleNum = int(staticFile.readline())
-size = float(staticFile.readline())
-cellSize = int(staticFile.readline())
-rc = float(staticFile.readline())
-edges = staticFile.readline() != 'false\n'
-for x in staticFile:
-  rawParticle = x.split(' ')
-  particles.append(Particle(rawParticle[0], float(rawParticle[2]), float(rawParticle[3]), rawParticle[1]))
-# print(particles)
+def argumentParser():
+  parser = argparse.ArgumentParser(
+      description='This program shows data from .\n', formatter_class=RawTextHelpFormatter)
 
-outputFile = open("data/vecins.txt", "r")
-endTime = int(outputFile.readline())
-for x in outputFile:
-  vecinsIds = x.split(' ')
-  vecinsIds.pop()
-  vecinsIds = list(map(lambda idStr : int(idStr), vecinsIds))
-  particleId = vecinsIds.pop(0)
-  particles[particleId].vecins = vecinsIds
+  parser.add_argument(
+      '--staticFile',
+      help="Path to the static data file.",
+      default='data/output.txt'
+  )
 
-idAnalized = 0
+  parser.add_argument(
+      '--neighboursFile',
+      help="Path to the neighbours file.",
+      default='data/vecins.txt'
+  )
+  
+  parser.add_argument(
+      '--interactable',
+      help="Test flag to show graphics and check error\n\n",
+      action='store_true'
+  )
 
-x = list(map(lambda particle : particle.posX, particles))
-y = list(map(lambda particle : particle.posY, particles))
-s = list(map(lambda particle : particle.radius * 100, particles))
-c = list(map(lambda particle : 'black', particles))
-c[idAnalized] = 'g'
-for vecin in particles[idAnalized].vecins:
-  c[vecin] = 'r'
-# print(particles[idAnalized].vecins)
-# c = list(map(lambda particle : 'black' if particle.posX>rc or particle.posY>rc else 'red', particles))
+  return parser
+
+
 
 def redraw(newId):
   plt.cla()
@@ -95,7 +88,6 @@ def redraw(newId):
 
 
 
-fig, ax = plt.subplots()
 
 def on_pick(event):
   print('Selected', particles[event.ind[0]])
@@ -105,9 +97,50 @@ def on_pick(event):
   redraw(event.ind[0])
   fig.canvas.draw()
 
-fig.canvas.mpl_connect('pick_event', on_pick)
 
-redraw(0)
-# my_updater.add_ax(ax, ['size', 'pick_event'])  # scatter plot, only marker size
-plt.show()
+if __name__ == "__main__":
+  # get parser
+  parsedArgs = argumentParser().parse_args()
 
+  my_updater = MarkerUpdater()
+
+  staticFile = open(parsedArgs.staticFile, "r")
+  particles = list()
+  particleNum = int(staticFile.readline())
+  size = float(staticFile.readline())
+  cellSize = int(staticFile.readline())
+  rc = float(staticFile.readline())
+  edges = staticFile.readline() != 'false\n'
+  for x in staticFile:
+    rawParticle = x.split(' ')
+    particles.append(Particle(rawParticle[0], float(
+        rawParticle[2]), float(rawParticle[3]), rawParticle[1]))
+  # print(particles)
+
+  outputFile = open(parsedArgs.neighboursFile, "r")
+  endTime = int(outputFile.readline())
+  for x in outputFile:
+    vecinsIds = x.split(' ')
+    vecinsIds.pop()
+    vecinsIds = list(map(lambda idStr: int(idStr), vecinsIds))
+    particleId = vecinsIds.pop(0)
+    particles[particleId].vecins = vecinsIds
+
+  idAnalized = 0
+
+  x = list(map(lambda particle: particle.posX, particles))
+  y = list(map(lambda particle: particle.posY, particles))
+  s = list(map(lambda particle: particle.radius * 100, particles))
+  c = list(map(lambda particle: 'black', particles))
+  c[idAnalized] = 'g'
+  for vecin in particles[idAnalized].vecins:
+    c[vecin] = 'r'
+  # print(particles[idAnalized].vecins)
+  # c = list(map(lambda particle : 'black' if particle.posX>rc or particle.posY>rc else 'red', particles))
+
+  fig, ax = plt.subplots()
+  fig.canvas.mpl_connect('pick_event', on_pick)
+
+  redraw(0)
+  # my_updater.add_ax(ax, ['size', 'pick_event'])  # scatter plot, only marker size
+  plt.show()
